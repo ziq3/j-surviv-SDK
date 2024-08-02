40ffe6afd13db183705eb036814648a5e343546b
ntuanvu89 <nguyentuanvuk62@gmail.com>
2024-08-02 15:51:47 +0700

    [CHANGE] event name - [ADD] shortest path algorithm

--- CodeFest2024/src/main/java/jsclub/codefest2024/Node.java
+++ CodeFest2024/src/main/java/jsclub/codefest2024/Node.java
@@ -1,13 +1,17 @@
 package jsclub.codefest2024;
 
+import static java.lang.Math.abs;
+
 public class Node {
     public int x;
     public int y;
+    public int g;
 
     public Node() {}
-    public Node(int x, int y) {
+    public Node(int x, int y, int g) {
         this.x = x;
         this.y = y;
+        this.g = g;
     }
 
     public int getX() {
@@ -15,6 +19,14 @@ public class Node {
     }
     public int getY() {
         return y;
+    }
+
+    public int mahattanDistance(Node x, Node y) {
+        return abs(x.x - y.x) + abs(x.y - y.y);
+    }
+
+    public int calculateF(Node x, Node y) {
+        return x.g + mahattanDistance(x, y);
     }
 
     public void setPosition(int x, int y) {
--- CodeFest2024/src/main/java/jsclub/codefest2024/algorithm/ShortestPath.java
+++ CodeFest2024/src/main/java/jsclub/codefest2024/algorithm/ShortestPath.java
@@ -3,12 +3,89 @@ package jsclub.codefest2024.algorithm;
 import jsclub.codefest2024.Node;
 import jsclub.codefest2024.sdk.model.GameMap;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
+import static java.lang.Math.abs;
 
 public class ShortestPath {
-    // @Vu
     // The algorithm to find the shortest path from the current node to the target node
     public static String getShortestPath(GameMap gameMap, List<Node> restrictedNodes, Node current, Node target) {
-        return "";
+        int Dx[] = {-1, 1, 0, 0};
+        int Dy[] = {0, 0, -1, 1};
+
+        int mapSize = gameMap.getMapSize();
+
+        ArrayList<ArrayList<Integer>> isRestrictedNodes = new ArrayList<>(mapSize + 1);
+        ArrayList<ArrayList<Integer>> g = new ArrayList<>(mapSize + 1);
+        ArrayList<ArrayList<Integer>> trace = new ArrayList<>(mapSize + 1);
+
+        for (int i = 0; i < mapSize + 1; i++) {
+            isRestrictedNodes.add(new ArrayList<>(mapSize + 1));
+            g.add(new ArrayList<>(mapSize + 1));
+            trace.add(new ArrayList<>(mapSize + 1));
+
+            for (int j = 0; j < mapSize + 1; j++) {
+                isRestrictedNodes.get(i).add(0);
+                g.get(i).add(-1);
+                trace.get(i).add(-1);
+            }
+        }
+
+        for (Node point : restrictedNodes) {
+            if (point.x >= 1 && point.x <= mapSize && point.y >= 1 && point.y <= mapSize) {
+                isRestrictedNodes.get(point.x).set(point.y, 1);
+            }
+        }
+
+        PriorityQueue<Node> openSet = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node n1, Node n2) {
+                return Integer.compare(n1.calculateF(n1, target), n2.calculateF(n2, target));
+            }
+        });
+
+        openSet.add(new Node(current.x, current.y, 0));
+        g.get(current.x).set(current.y, 0);
+
+        String ans = "";
+
+        while (!openSet.isEmpty()) {
+            Node u = openSet.poll();
+
+            if (u.x == target.x && u.y == target.y) {
+                Node v = target;
+                while (v.x != current.x || v.y != current.y) {
+                    int dir = trace.get(v.x).get(v.y);
+                    if (dir == 0) ans = ans + 'l';
+                    else if (dir == 1) ans = ans + 'r';
+                    else if (dir == 2) ans = ans + 'd';
+                    else ans = ans + 'u';
+                    v.x -= Dx[dir];
+                    v.y -= Dy[dir];
+                }
+
+                ans = new StringBuilder(ans).reverse().toString();
+                break;
+            }
+
+            for (int dir = 0; dir < 4; dir++) {
+                int x = u.x + Dx[dir];
+                int y = u.y + Dy[dir];
+
+                if (x < 1 || y < 1 || x > mapSize || y > mapSize) continue;
+                if (isRestrictedNodes.get(x).get(y) == 1) continue;
+                int cost = g.get(u.x).get(u.y) + 1;
+                if (g.get(x).get(y) == -1 || g.get(x).get(y) > cost) {
+                    g.get(x).set(y, cost);
+                    trace.get(x).set(y, dir);
+                    openSet.add(new Node(x, y, cost));
+                }
+            }
+        }
+
+        return ans;
     }
 }
