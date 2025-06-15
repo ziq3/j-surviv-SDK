ae3b7f9812c628d3e028ede64f98cfdcf6cba560
unknown <Tuantumat203@gmail.com>
2025-06-16 00:14:40 +0700

    Fix Dame, On-hit points SHOTGUN. Fix Range MACE

--- src/main/java/jsclub/codefest2024/sdk/Main.java
+++ src/main/java/jsclub/codefest2024/sdk/Main.java
@@ -1,3 +1,5 @@
+package jsclub.codefest2024.sdk;
+
 import io.socket.emitter.Emitter;
 import jsclub.codefest2024.sdk.Hero;
 import jsclub.codefest2024.sdk.model.GameMap;
@@ -6,15 +8,15 @@ import java.io.IOException;
 
 public class Main {
     private static final String SERVER_URL = "https://cf25-server-staging.jsclub.dev";
-//    private static final String SERVER_URL = "http://192.168.123.126:3000";
-    private static final String GAME_ID = "159910";
+    //    private static final String SERVER_URL = "http://192.168.123.126:3000";
+    private static final String GAME_ID = "100348";
     private static final String PLAYER_NAME = "phi";
     private static final String SECRET_KEY = "sk-zsSlPQT_R_-vRYEi0FYVhA:rQ2O70MyHthadd-rcUryK3i4rtsWp2pG1UHrWktTNz6vhBJC8fnzpJZ-wfHKk8aTJYb8nhcY97DmwsC2NZDXZA";
 //    private static final String SECRET_KEY = "sk-bKmGDraDRVSV1opmx1gZ7Q:YYrurLaSNj8-FVHpMiYtfWn5quvLda1NevFOe6d41QL3JCKDl8WVy5xsi34fXdjWS3SRQIdGtCNBD8Hk-gkWHw";
 
     public static void main(String[] args) throws IOException {
         Hero hero = new Hero(GAME_ID, PLAYER_NAME,SECRET_KEY);
-
+        System.out.println("Hello");
         Emitter.Listener onMapUpdate = new Emitter.Listener() {
             @Override
             public void call(Object... args) {
@@ -25,7 +27,9 @@ public class Main {
                 System.out.println(gameMap);
             }
         };
+        System.out.println("Haha");
         hero.setOnMapUpdate(onMapUpdate);
+        System.out.println("Hehe");
         hero.start(SERVER_URL);
     }
 }
